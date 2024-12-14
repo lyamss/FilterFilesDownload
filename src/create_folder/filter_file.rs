8d45bcc9c2d64dcefffb76d5cfcf1fbc7775f365
Lyamss <161554647+lyamss@users.noreply.github.com>
2024-12-14 01:13:12 +0100

    resolve #3

--- src/create_folder/filter_file.rs
+++ src/create_folder/filter_file.rs
@@ -27,62 +27,7 @@ use std::path::PathBuf;
 
                     if path.is_file() || path.is_dir()
                     {
-
-                        if let Some(extension) = path.extension() 
-                        {
-
-                            
-                            if let Some(ext_str) = extension.to_str() 
-                            {
-                                
-                                    let ext_lower: String = ext_str.to_lowercase();
-
-
-                                    if let Some(file_name) = path.file_name()
-                                    {
-                                        let sub_dirs: Vec<(&str, Vec<&str>)> = vec![
-                                            ("Audio", vec!["mp3", "wav", "flac", "aac", "aiff"]),
-                                            ("Documents", vec!["pdf", "txt", "csv", "md"]),
-                                            ("Movies", vec!["mp4", "mov", "mpeg"]),
-                                            ("Images", vec!["gif", "png", "jpg", "jpeg", "svg"]),
-                                            ("Other", vec![]),
-                                        ];
-
-
-                                        if let Some((dest_folder, _)) = sub_dirs.iter().find(|&&(_, ref allowed_exts)| allowed_exts.contains(&ext_lower.as_str())) 
-                                        {
-                                            let destination_path:PathBuf = base_dir.join(dest_folder).join(file_name);
-                                            if let Err(e) = fs::rename(&path, &destination_path) 
-                                            {
-                                                eprintln!("Error moving file '{}': {}", path.display(), e);
-                                            }
-                                        } 
-
-                                        else 
-                                        {
-                                            let destination_path:PathBuf = base_dir.join("Other").join(file_name);
-                                            if let Err(e) = fs::rename(&path, &destination_path) 
-                                            {
-                                                eprintln!("Error moving file '{}': {}", path.display(), e);
-                                            }
-                                        }
-
-                                    }
-                            }
-                        }
-
-                        else 
-                        {
-                            if let Some(p) = path.file_name()
-                            {
-                                let destination_path:PathBuf = base_dir.join("Other").join(p);
-                                if let Err(e) = fs::rename(&path, &destination_path) 
-                                {
-                                    drop(e);
-                                    // eprintln!("Error moving file '{}': {}", path.display(), e);
-                                }
-                            }
-                        }
+                        move_file(path, &base_dir);
                     }
                 }
             }
@@ -91,5 +36,68 @@ use std::path::PathBuf;
         else 
         {
             println!("Unable to find the downloads folder");
+        }
+    }
+
+
+
+
+    
+    fn move_file(path :&PathBuf, base_dir: &PathBuf)
+    {
+        if let Some(extension) = path.extension() 
+        {
+                            
+            if let Some(ext_str) = extension.to_str() 
+            {
+                
+                let ext_lower: String = ext_str.to_lowercase();
+
+
+                if let Some(file_name) = path.file_name()
+                {
+
+                    let sub_dirs: Vec<(&str, Vec<&str>)> = vec![
+                        ("Audio", vec!["mp3", "wav", "flac", "aac", "aiff"]),
+                        ("Documents", vec!["pdf", "txt", "csv", "md"]),
+                        ("Movies", vec!["mp4", "mov", "mpeg"]),
+                        ("Images", vec!["gif", "png", "jpg", "jpeg", "svg"]),
+                        ("Other", vec![]),
+                    ];
+
+
+                    if let Some((dest_folder, _)) = sub_dirs.iter().find(|&&(_, ref allowed_exts)| allowed_exts.contains(&ext_lower.as_str())) 
+                    {
+                        let destination_path:PathBuf = base_dir.join(dest_folder).join(file_name);
+                        if let Err(e) = fs::rename(&path, &destination_path) 
+                        {
+                            eprintln!("Error moving file '{}': {}", path.display(), e);
+                        }
+                    }
+
+                    else 
+                    {
+                        let destination_path:PathBuf = base_dir.join("Other").join(file_name);
+                        if let Err(e) = fs::rename(&path, &destination_path) 
+                        {
+                            eprintln!("Error moving file '{}': {}", path.display(), e);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        else 
+        {
+            if let Some(p) = path.file_name()
+            {
+                let destination_path:PathBuf = base_dir.join("Other").join(p);
+                if let Err(e) = fs::rename(&path, &destination_path) 
+                {
+                    drop(e);
+                    // eprintln!("Error moving file '{}': {}", path.display(), e);
+                }
+            }
         }
     }
